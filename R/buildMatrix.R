349d1844c3437a1efb94e0285018d2a4b8fbc391
Joseph Lewis <joelewis@ucdavis.edu>
2017-10-03 16:49:27 -0700

    Cleanup of buildMatrix.R
    cleaned up buildMatrix.R in coding and updated the documentation to be more clear and concise

--- R/buildMatrix.R
+++ R/buildMatrix.R
@@ -1,17 +1,27 @@
 
-buildMatrix <- function(ratingsIn,NAval=0){
-   # deal with possible factors
-   dmax <- function(d) {
-      if (is.factor(d)) return(length(levels(d)))
-      max(d)
-   }
-   users = ratingsIn[,1]
-   movies = ratingsIn[,2]
-   rating = ratingsIn[,3]
-   newMatrix = matrix(NAval, 
-          nrow = dmax(users), ncol = dmax(movies))
-   for(rows in 1:nrow(ratingsIn)){
-       newMatrix[ratingsIn[rows,1],ratingsIn[rows,2]] = ratingsIn[rows,3]
-   }
-   return (newMatrix)
+buildMatrix <- function(ratingsIn,NAval=0) {
+
+  # sub-routine to deal with possible factors
+  dmax <- function(d) {
+  
+    if (is.factor(d)) {
+      return(length(levels(d)))
+    }
+  
+    return(max(d))
+  }
+  
+  # begin formulating the new matrix  
+  users = ratingsIn[,1]
+  items = ratingsIn[,2]
+  ratings = ratingsIn[,3]
+ 
+  newMatrix = matrix(NAval, nrow = dmax(users), ncol = dmax(items))
+ 
+  # populate the matrix with the given user ratings
+  for(row in 1:nrow(ratingsIn)) {
+    newMatrix[users[row],items[row]] = ratings[row]
+  }
+ 
+  return(newMatrix)
 }
