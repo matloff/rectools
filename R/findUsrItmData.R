14b71fd9c91b0c0a94f8c23feeed9ebb7ebcc931
Norm Matloff <matloff@cs.ucdavis.edu>
2017-10-18 14:32:17 -0700

    better doc

--- R/findUsrItmData.R
+++ R/findUsrItmData.R
@@ -1,6 +1,8 @@
 
 # utility to read in raw data in standard format,
+
 #    (user ID, item ID, rating)
+
 # and form an R list for user data, with class 'usrData'; each
 # element of the list will be of class 'usrDatum', and will have
 # components as seen in 'value' below 
@@ -27,33 +29,43 @@
 #       ratings: vector of ratings made by this user
 #       itms: IDs of items rated by this user
 #       cvrs:  covariate data for this user, if any
-#       cats:  item category data for this user, if any; i-th element
+#       cats:  item category data for this user, if any; j-th element
 #              is proportion of items rated by this user that are 
-#              in category i
+#              in category j; e.g. for this user, 0.22 of the movies he
+#              rates are comedies, 0.07 are historical dramas, etc.; 
+#              again, sum could be greater or less than 1.00
 
 formUserData <- function(ratingsIn,usrCovs=NULL,itmCats=NULL,fileOut='') {
+
    # covariates, if any, should be in usrCovs; check to see if user has
    # included them
    if (ncol(ratingsIn) > 3)
       stop('ratingsIn more than 3 columns')
 
-   ## IMPORTANT NOTE: in order to work in xval, etc. we need to abandon
-   ## the idea of having the user IDs start at 1 and be consecutive;
-   ## instead, we will just use the ID numbers as list indices; e.g. if we
-   ## have users numbered 2,8,85 then retval below will consist of
-   ## retval[[2]], retval[[8]] and retval[[85]]
+   # IMPORTANT NOTE: in order to work in cross-validation, etc. we need
+   # to abandon the idea of having the user IDs start at 1 and be
+   # consecutive; instead, we will just use the ID numbers as list
+   # indices; e.g. if we have users numbered 2,8,85 then retval below
+   # will consist of retval[[2]], retval[[8]] and retval[[85]]
 
    # rownums[[i]] will be the row numbers in ratingsIn belonging to user i
-   ## rownums <- split(1:nrow(ratingsIn),ratingsIn[,1])
    rownums <- split(1:nrow(ratingsIn),ratingsIn[,1])
    nusers <- length(rownums)
+   nitems <- length(unique(ratingsIn[,2])))
+
    if (!is.null(itmCats)) {
+      # must convert from a data frame row to a numeric vector
       itmCats <- as.matrix(itmCats)
-      nitems <- nrow(itmCats)
+      nitms <- nrow(itmCats)
+      if (nitms != nitems) stop('itmCats has too many/few rows')
    }
-   retval <- vector('list',length(rownums))
+
+   # rete will ultimately be the return value, a list of lists as
+   # described above.
+   retval <- vector('list',length(rownums))  
+
    for (i in 1:nusers) {
-      whichrows <- rownums[[i]]
+      whichrows <- rownums[[i]]  # row nums in ratingsIn for user i
       userID <- ratingsIn[whichrows[1],1]
       # start building usrDatum object for this user
       retval[[userID]] <- list()
@@ -61,8 +73,11 @@ formUserData <- function(ratingsIn,usrCovs=NULL,itmCats=NULL,fileOut='') {
       retval[[userID]]$itms <- ratingsIn[whichrows,2]
       retval[[userID]]$ratings <- ratingsIn[whichrows,3]
       if (!is.null(usrCovs))
+         # change from data frame row to numeric vector
          retval[[userID]]$cvrs <- as.numeric(usrCovs[userID,])
       if (!is.null(itmCats)) {
+         # form tmp, a boolean vector indicating which items were or
+         # were not rated by this user
          tmp <- rep(0,nitems)
          tmp[retval[[userID]]$itms] <- 1
          # form vector whose j-th element will be the count of items
@@ -78,13 +93,12 @@ formUserData <- function(ratingsIn,usrCovs=NULL,itmCats=NULL,fileOut='') {
    retval
 }
 
-# construct a new object of class 'usrDatum'
-
-formUserDatum <- function(itms,ratings,userID=NULL) {
-    obj <- list(itms = itms, ratings=ratings,userID=userID)
-    class(obj) <- 'usrDatum'
-    obj
-}
+## # construct a new object of class 'usrDatum'
+## formUserDatum <- function(itms,ratings,userID=NULL) {
+##     obj <- list(itms = itms, ratings=ratings,userID=userID)
+##     class(obj) <- 'usrDatum'
+##     obj
+## }
 
 # utility:  find input row for a given user, item
 findInputRow <- function(ratingsIn,usrID,itmID) {
