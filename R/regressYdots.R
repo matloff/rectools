8afc9a1f6e059cec279e423a7e7afa92e8037c84
Norm Matloff <matloff@cs.ucdavis.edu>
2018-02-25 23:01:45 -0800

    improved code, comments, started man pg

--- R/regressYdots.R
+++ R/regressYdots.R
@@ -1,21 +1,28 @@
 
-# motivation:
+# Motivation:
 
-# the MM and MLE models have
+# In the MM and MLE latent-factor models in this package, we have
  
 #    Y_ij = mu + alpha_i + beta_j + eps
  
-# we then estimate the mu, alpha_i and beta_j using MM or MLE; in the MM
-# case, the estimated alpha_i is the mean of all known ratings from user
-# i, minus mu, which is estimated by the overall mean of all ratings
- 
-# but the above equation looks like a regression equation, so we might
+# But the above equation looks like a regression equation, so we might
 # treat the estimates of alpha_i and beta_j, which are the per-user and
 # per-item sample means Yi. and Y.j, as predictor variables in a
-# regression context; it seems intuitive that Yi. and Y.j would be
-# reasonable candidtes for predictors, and of course one can add
-# along with any covariates, whether user-specific, item-specific or
-# pertaining to both user and item 
+# regression context, i.e. we predict Y_ij from:
+
+#    Yi.
+#    Y.j
+#    optional covariates depending on i, j or i and j jointly
+
+# For instance, for the MovieLens data, covariates for user i might be 
+# age and gender, those for item j might be genre, and a joint
+# covariate might be user i's general liking for the genre of item j.
+
+# It seems intuitive that Yi. and Y.j would be
+# reasonable candidates for predictors. 
+
+# Any regression model can be used, e.g. linear, logistic, random
+# forests, neural networks, etc.
 
 # arguments:
 
@@ -23,35 +30,50 @@
 #               (userID, itemID, rating, covariates)
 #    regModel: the function to use for regression, such as lm() or
 #              randomForest()
-#    ydotsObj: output from trainMM(), to get the user and item means
+#    ydotsObj: output from trainMM(), to get the user and item means; if
+#              NULL, will be generated here
 #    rmArgs: regression model arguments, e.g. number of nearest
 #            neighbors; expressed as a quoted string; not implemented yet
 
+# IMPORTANT NOTE (repeated here from trainMM.R):
+# user and item IDs may not be consecutive; even if they are
+# consecutive in the original, if we do cross-validation, this 
+# may not be the case; so we switch to character IDs
+
 regressYdots <- function(ratingsIn,regModel='lm',ydotsObj=NULL,rmArgs=NULL) 
 {
-   if (is.null(ydotsObj)) ydotsObj <- trainMM(ratingsIn)
+   covs <- as.matrix(ratingsIn[,-(1:3)])  # NULL if no covariates
+   if (!is.null(covs)) {
+      covs <- as.data.frame(covs)
+      colnames(covs) <- names(ratingsIn[,-(1:3)]) 
+   }
+   if (is.null(ydotsObj)) ydotsObj <- trainMM(ratingsIn[,1:3])
    usrMeans <- ydotsObj$usrMeans
    itmMeans <- ydotsObj$itmMeans
-   covs <- as.matrix(ratingsIn[,-(1:3)])  # NULL if no covariates
-   # next 2 lines needed due to indexing of usrMeans and itmMeans; see
-   # comment in trainReco()
-   usrsChar <- as.character(ratingsIn[,1])
-   itmsChar <- as.character(ratingsIn[,2])
-   uMeans <- usrMeans[usrsChar]
-   iMeans <- itmMeans[itmsChar]
-   # uMeans, iMeans have length same as nrow(ratingsIn)
+   # as explained above, usrMeans and itmMeans are indexed by character
+   # versions of user/item IDs; thus again, the next 2 lines are needed 
+   # due to character indexing of usrMeans and itmMeans
+   usrsInput <- as.character(ratingsIn[,1])
+   itmsInput <- as.character(ratingsIn[,2])
+   uMeans <- usrMeans[usrsInput]
+   iMeans <- itmMeans[itmsInput]
+   # uMeans, iMeans have length = nrow(ratingsIn); e.g. i-th element of
+   # uMeans is Yi.
    xy <- data.frame(uMeans,iMeans,covs,ratingsIn[,3])
    x <- data.frame(uMeans,iMeans,covs)
    y <- ratingsIn[,3]
    names(xy) <- c('uMeans','iMeans',names(covs),'rats')
-   if (regModel == 'lm') return(lm(rats ~ .,data=xy)) 
+   if (regModel %in% c('lm','rf','ctree')) {
+      cmd <- paste0(regModel,'(')
+      cmd <- paste0(cmd,'rats ~ .,data=xy')
+      if (!is.null(rmArgs)) {
+         cmd <- paste0(cmd,'.',rmArgs,')')
+      } else cmd <- paste0(cmd,')')
+      return(eval(parse(text=cmd)))
+   }
    if (regModel == 'rf') {
       require(randomForest)
       return(randomForest(x,y))
    } 
-   if (regModel == 'ctree') {
-      require(partykit)
-      return(ctree(rats ~ .,data=xy))
-   }
 }
 
