767a9d28f9f4203a2f7053ada92451cb0a9b084c
Melanie Zhang <melanizhang@gmail.com>
2018-02-28 20:28:23 -0800

    add heatmap & covariate plots

--- R/explore.R
+++ R/explore.R
@@ -1,4 +1,12 @@
-explore <- function(ratingsIn) {
+# generates a sequence of plots exploring input data
+
+# arguments:
+
+#   ratingsIn: input data, with first cols (userID,itemID,rating,
+#              covariates)
+#   plotcovs: if True, generate plots for covariates (if there are any)
+
+explore <- function(ratingsIn, plotcovs=TRUE) {
   users <- ratingsIn[,1]
   items <- ratingsIn[,2]
   ratings <- ratingsIn[,3]
@@ -6,6 +14,7 @@ explore <- function(ratingsIn) {
   Y.j <- tapply(ratings,items,mean) # means of all ratings per item
   Ni. <- tapply(ratings,users,length) # number of ratings per user
   N.j <- tapply(ratings,users,length) # number of ratings per item
+  
   # histograms
   hist(ratings)
   par(ask=TRUE)
@@ -15,6 +24,28 @@ explore <- function(ratingsIn) {
   hist(N.j)
   
   # scatterplots
-  plot(sapply(users, function(x) Yi.[x]), ratings, main="Yi. vs Yij", xlab="Yi.", ylab="Yij")
-  plot(sapply(items, function(x) Y.j[x]), ratings, main="Y.j vs Yij", xlab="Y.j", ylab="Yij")
+  userMeans <- sapply(users, function(x) Yi.[x])
+  itemMeans <- sapply(items, function(x) Y.j[x])
+  
+  plot(userMeans, ratings, main="Yi. vs Yij", xlab="Yi.", ylab="Yij")
+  plot(itemMeans, ratings, main="Y.j vs Yij", xlab="Y.j", ylab="Yij")
+  
+  # heatmap
+  palette <- colorRampPalette(c('blue','red'))
+  ratColors <- palette(10)[as.numeric(cut(ratings, breaks=10))]
+  plot(userMeans, itemMeans, col=ratColors, main="Ratings Heatmap", xlab="Yi.", ylab="Y.j")
+  
+  # if there are covariates
+  if (plotcovs && ncol(ratingsIn) > 3) {
+    for (i in 4:ncol(ratingsIn)) {  # plot a histogram/barchart for each covariate column
+      covName = colnames(ratingsIn)[i]
+      
+      if (is.numeric(ratingsIn[,i])) {
+        hist(ratingsIn[,i], main=paste("Histogram of",covName), xlab=colnames(ratingsIn)[i])
+      }
+      else {
+        barplot(table(ratingsIn[,i]), main=paste("Bar plot of", covName), xlab=colnames(ratingsIn)[i])
+      }
+    }
+  }
 }
