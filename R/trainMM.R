fb7c793522991ddb91260f117d342cf42d554d4a
Norm Matloff <matloff@cs.ucdavis.edu>
2018-03-31 23:00:56 -0700

    nonpar case works, incl. covs

--- R/trainMM.R
+++ R/trainMM.R
@@ -1,11 +1,6 @@
 
 # Method of Moments approach
 
-# TODO:
-
-#    accommodate matrices in addition to data frames, in order to all
-#    use of bigmemory package
-#    
 #  for simplicity, the following discussion is at the population level
 
 #  basic model is
@@ -54,14 +49,13 @@
 
 #   S3 class of type "ydots", with components:
 
-#      Y..: grand mean, est of mu 
-#      Yi.: vector of mean ratings for each user, ests. of alpha_i
-#      Y.j: vector of mean ratings for each item, ests. of betaa_j
+#      alphai: vector of ests. of alpha_i
+#      betaj: vector of ests. of beta_j
 #      lmout: object returned by running the regression analysis 
 #      Ni.: vector of number of ratings by each user
 #      N.j: vector of number of ratings of each item
 
-trainMM <- function(ratingsIn,userCovsStartCol=NULL,itemCovsStartCol=NULL)
+trainMM <- function(ratingsIn)
 {
   users <- ratingsIn[,1]
   items <- ratingsIn[,2]
@@ -73,58 +67,41 @@ trainMM <- function(ratingsIn,userCovsStartCol=NULL,itemCovsStartCol=NULL)
   items <- as.character(items)
   ratings <- ratingsIn[,3]
   nms <- names(ratingsIn)
+
   n <- nrow(ratingsIn)
-  userRows <- split(1:n,users)
-  itemRows <- split(1:n,items)
-  Y.. <- mean(ratings)  # overall mean
+  userRowGrps <- split(1:n,users)
+  itemRowGrps <- split(1:n,items)
 
-  uimean <- function(uirow) mean(ratings[uirow)
-  Yi. <- sapply(userRows,uimean)
-  Y.j <- sapply(itemRows,uimean)
+  uimean <- function(uirowgrp) mean(ratings[uirowgrp])
+  Yi. <- sapply(userRowGrps,uimean)
+  Y.j <- sapply(itemRowGrps,uimean)
 
-  Yi. <- tapply(ratings,users,mean) # means of all ratings per user
-  Y.j <- tapply(ratings,items,mean) # means of all ratings per item
-  ydots <- list(grandMean=Y..,usrMeans=Yi.,itmMeans=Y.j)
-  ydots$trainingUsers <- unique(users)
-  ydots$trainingItems <- unique(items)
+  xmeans <- function(uirowgrp) colMeans(ratingsIn[uirowgrp,-(1:3),drop=FALSE])
+  Xi. <- t(sapply(userRowGrps,xmeans))
+  X.j <- t(sapply(itemRowGrps,xmeans))
+
   haveCovs <- ncol(ratingsIn) > 3
   if (haveCovs) {
-     covCols <- getCovCols(userCovsStartCol,itemCovsStartCol,ncol(ratingsIn))
-     usrCovCols <- covCols[[1]]
-     itmCovCols <- covCols[[2]]
-     ydots$usrCovCols <- usrCovCols  # vector of column numbers
-     ydots$itmCovCols <- itmCovCols  # vector of column numbers
-     # as noted above, center the covs
-     tmp <- scale(ratingsIn[,-(1:3)],scale=FALSE)
-     ratingsIn[,-(1:3)] <- tmp
-     # need to record the centering process in order to use predict()
-     # later, so the mean of each covariate is saved here
-     ydots$covMeans <- attr(tmp,'scaled:center')
-     # regress ratings against covariates, no constant term
-     # NOTE:  could do a weighted least squares, using the Ni, 
-     # but treating the latter is random too, not needed;
-     # user covs first, if any
-     if (!is.null(usrCovCols)) {
-        nms3 <- nms[3]
-        cmd <- paste(nms3, ' - mean(',nms3,') ~ .-1',sep='')
-        frml <- as.formula(cmd)
-        frml <- as.formula(paste(nms3,'-~ .-1'))
-        lmout <- lm(frml,data=ratingsIn[,c(3,usrCovCols)])
-        ydots$lmoutUsr <- lmout
-     }
-     # now item covs, if any
-     if (!is.null(itmCovCols)) {
-        nms3 <- nms[3]
-        cmd <- paste(nms3, ' - mean(',nms3,') ~ .-1',sep='')
-        frml <- as.formula(cmd)
-        lmout <- lm(frml,data=ratingsIn[,c(3,itmCovCols)])
-        ydots$lmoutItm <- lmout
-     }
-     Ni. <- tapply(ratings,users,length) # number of ratings per user
-     ydots$Ni. <- Ni.
+  # get gammahat and associated 'lm' object
+     cmd <- paste('lmout <- lm(', nms[3],
+               ' ~ .,data=ratingsIn[,-(1:2)])', sep='')
+     eval(parse(text=cmd))
+     predsa <- predict(lmout,as.data.frame(Xi.))
+     predsb <- predict(lmout,as.data.frame(X.j))
+  } else {
+     predsa <- 0
+     predsb <- 0
+  }
+  
+  alphai <- Yi. - predsa
+  betaj <- Y.j - predsb
+
+  ydots <- list(alphai=alphai,betaj=betaj)
+  if (haveCovs) ydots$lmout <- lmout else ydots$Y.. <- mean(ratings)
+  Ni. <- tapply(ratings,users,length) # number of ratings per user
+  ydots$Ni. <- Ni.
      N.j <- tapply(ratings,users,length) # number of ratings per item
      ydots$N.j <- N.j
-  } 
   class(ydots) = 'ydotsMM'
   invisible(ydots)
 }
@@ -149,59 +126,23 @@ predict.ydotsMM = function(ydotsObj,testSet)
    # index, i.e. w['b'] vs. w[28]
    ts1 <- as.character(testSet[,1])  # user IDs, char form
    ts2 <- as.character(testSet[,2])  # item IDs, char form
-   usrMeans <- ydotsObj$usrMeans[ts1]  # using named elements
-   itmMeans <- ydotsObj$itmMeans[ts2]  # using named elements
+   usrAlphas <- ydotsObj$alphai[ts1]  # using named elements
+   itmBetas <- ydotsObj$betaj[ts2]  # using named elements
    # make all terms in sums below have consistent element names!;
    # essentially, we are naming the elements of both usrMeans and
    # itmMeans after the elements of testSet[,1]
-   names(itmMeans) <- ts1
-   # now start building the prediction
+   names(itmBetas) <- ts1
    nTest <- nrow(testSet)
    haveCovs <- ncol(testSet) > 2
+   # build the predictions vector
+   preds <- vector(length=nTest)
    if (!haveCovs) {
-      pred <- usrMeans + itmMeans - ydotsObj$grandMean
+      preds[] <- ydotsObj$Y..
+   } else {
+      preds[] <- predict(ydotsObj$lmout,testSet) 
    }
-   else {
-      # where are the covariates?
-      usrCovCols <- ydotsObj$usrCovCols 
-      itmCovCols <- ydotsObj$itmCovCols
-      # shift left due to no Ratings column
-      if (!is.null(usrCovCols)) usrCovCols <- usrCovCols - 1 
-      if (!is.null(itmCovCols)) itmCovCols <- itmCovCols - 1 
-      # must center the covariates, using the same centering information
-      # used in ydotsObj
-      colmeans <- ydotsObj$covMeans
-      colmeans <- matrix(rep(colmeans,nTest),nrow=nTest,byrow=TRUE)
-      testSet[,-(1:2)] <- testSet[,-(1:2)] - colmeans
-
-      # start with the mu term
-      Y.. <- ydotsObj$grandMean
-      pred <- rep(Y..,nTest) 
-      
-      # now the alpha term:
-      if(!is.null(usrCovCols)) {
-         predalpha <- 
-            predict(ydotsObj$lmoutUsr,testSet[,usrCovCols,drop=FALSE])
-      } else {
-         Yi. <- ydotsObj$usrMeans
-         testUsrNames <- as.character(testSet[,1])
-         predalpha <- Yi.[testUsrNames] - Y..
-      }
-      
-      # and beta:
-      if(!is.null(itmCovCols)) {
-         predbeta <- 
-            predict(ydotsObj$lmoutItm,testSet[,itmCovCols,drop=FALSE])
-      } else {
-         Y.j <- ydotsObj$itmMeans
-         testItmNames <- as.character(testSet[,2])
-         predbeta <- Y.j[testItmNames] - Y..
-      }
-
-      # and finally
-      pred <- pred + predalpha + predbeta
-   }
-   names(pred) <- NULL  # use ordinal indexing
-   pred
+   preds <- preds + usrAlphas + itmBetas
+   names(preds) <- NULL  # use ordinal indexing
+   preds
 }
 
