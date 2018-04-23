885879c8ec8e7e4795e3456e5883bbff679689ea
Norman Matloff <normanmatloff@Normans-MacBook-Air.local>
2018-04-23 06:12:50 -0700

    still working on xvl

---
+++ R/trainRegYdots.R
@@ -0,0 +1,138 @@
+
+# Motivation:
+
+# In the MM and MLE latent-factor models in this package, we have
+ 
+#    Y_ij = mu + alpha_i + beta_j + eps
+ 
+# But the above equation looks like a regression equation, so we might
+# treat estimates of alpha_i and beta_j in the form of per-user and
+# per-item sample means Yi. and Y.j, as predictor variables in a
+# regression context, i.e. we predict Y_ij from:
+
+#    Yi.
+#    Y.j
+#    optional covariates depending on i, j or i and j jointly
+
+# For instance, for the MovieLens data, covariates for user i might be 
+# age and gender, those for item j might be genre, and a joint
+# covariate might be user i's general liking for the genre of item j.
+
+# It seems intuitive that Yi. and Y.j would be reasonable candidates for
+# predictors, regardless of whether the above model is correct.  (Note
+# that the model for the regression function amy still be correct; the
+# regression function of Y on alpha and beta does exist, where mu+alpha
+# and mu+beta are the limits of Yi. and Y.j
+
+# Any regression model can be used, e.g. linear, logistic, random
+# forests, neural networks, etc.
+
+# arguments:
+
+#    ratingsIn: the raw input data, each having the form 
+#               (userID, itemID, rating, covariates)
+#    regModel: the function to use for regression; current choices are
+#              'lm'; 'glm' with family = binomial; 'dn' (deepnet
+#              neural nets package); 'poly' (polyreg package)
+#              
+#    rmArgs: regression model arguments, e.g. number of nearest
+#            neighbors; expressed as a quoted string; not implemented yet
+
+# value:
+
+#    whatever the regression function returns
+
+# IMPORTANT NOTE (repeated here from trainMM.R):
+# user and item IDs may not be consecutive; even if they are
+# consecutive in the original, if we do cross-validation, this 
+# may not be the case; so we switch to character IDs
+
+# when predicting new observations, don't forget to call IDstoMeans() on
+# the new data
+
+trainRegYdots <- function(ratingsIn,regModel='lm',rmArgs=NULL) 
+{
+
+   x <- IDsToMeans(ratingsIn)
+   y <- ratingsIn[,3]
+   xy <- cbind(x,y)
+   names(xy) <- c(names(x),'rats')
+
+   result <- list(regModel=regModel,x=x, y=y)  # ultimately the return value
+   class(result) <- 'regYdots'
+
+   # perform the regression analysis
+   if (regModel %in% c('lm','glm')) {
+      cmd <- paste0(regModel,'(')
+      cmd <- paste0(cmd,'rats ~ .,data=xy')
+      if (!is.null(rmArgs)) {
+         cmd <- paste0(cmd,',',rmArgs,')')
+      } else cmd <- paste0(cmd,')')
+      regOut <- eval(parse(text=cmd))
+   } else if (regModel == 'dn') {
+      require(deepnet)
+     
+      x <- as.matrix(x)
+      x <- scale(x)
+      result$x <- x
+      cmd <- 'nn.train(x,y,'
+      cmd <- paste0(cmd,rmArgs,')')
+      regOut <- eval(parse(text=cmd))
+   } else if (regModel == 'poly') {
+      require(polyreg)
+      cmd <- 'polyFit(xy,'
+      cmd <- paste0(cmd,rmArgs,')')
+      regOut <- eval(parse(text=cmd))
+   } 
+
+   result$regOut <- regOut
+   result
+}
+
+# converts the first two columns of ratingsIn from user/item IDs to
+# user/item ratings means; copies the covs (columns 4+, if any); adjusts
+# column names accordingly
+IDsToMeans <- function(ratingsIn) 
+{
+   # handle covariates first
+   covs <- as.matrix(ratingsIn[,-(1:3)])  # NULL if no covariates
+   if (!is.null(covs)) {
+      covs <- as.data.frame(covs)
+      colnames(covs) <- names(ratingsIn[,-(1:3)]) 
+   }
+
+   users <- as.character(ratingsIn[,1])
+   items <- as.character(ratingsIn[,2])
+   ratings <- ratingsIn[,3]
+
+   # could add code to allow reusing previous computation
+   Ni. <- tapply(ratings,users,length) # number of ratings per user
+   N.j <- tapply(ratings,items,length) # number of ratings per item
+   usrMeans <- tapply(ratings,users,mean)
+   itmMeans <- tapply(ratings,items,mean)
+   
+   # as explained above, usrMeans and itmMeans are indexed by character
+   # versions of user/item IDs; thus again, the next 2 lines are needed 
+   # due to character indexing of usrMeans and itmMeans
+   usrsInput <- as.character(ratingsIn[,1])
+   itmsInput <- as.character(ratingsIn[,2])
+   uMeans <- usrMeans[usrsInput]
+   iMeans <- itmMeans[itmsInput]
+   # uMeans, iMeans have length = nrow(ratingsIn); e.g. i-th element of
+   # uMeans is Yi.
+   uN <- Ni.[usrsInput]
+   iN <- N.j[itmsInput]
+   means <- data.frame(uMeans=uMeans,iMeans=iMeans,uN=uN,iN=iN)
+   names(means) <- c('uMeans','iMeans','uN','iN')
+   x <- cbind(means,covs)
+   x
+}
+
+predict.regYdots <- function(rgydObj,newdata) 
+{
+   if (rgydObj$regModel != 'dn') {
+      predict(rgydObj$regOut,newdata)
+   } else {
+      nn.predict(rgydObj$regOut,newdata)
+   }
+}
---
+++ R/xvalRegYdots.R
@@ -0,0 +1,117 @@
+
+# value:
+
+#    accuracy values (MAE, RMS etc.)
+
+xvalRegYdots <- function(ratingsIn,regModel='lm',rmArgs=NULL,
+                   holdout=10000,printTimes=TRUE)
+{
+  ratIn = ratingsIn 
+ 
+  # split into random training and validation sets 
+  nrowRatIn = nrow(ratIn)
+  # training stage
+  testIdxs = sample(1:nrowRatIn,holdout)
+  trainSet = ratIn[-testIdxs, ]
+  trainRatings = trainSet[,3]
+  trainItems = trainSet[,2]
+  trainUsers = trainSet[,1]
+  tmp <- system.time(
+     ryout <- trainRegYdots(trainSet,regModel=regModel,rmArgs=rmArgs) 
+  )
+  if (printTimes) cat('training time: ',tmp,'\n')
+  # test stage
+  browser()
+  testA = ratIn[testIdxs,]
+  # need to convert to Ydots form
+  x <- IDsToMeans(testA)
+  if (regModel == 'dn') x <- scale(x)
+  testA <- cbind(x,ratIn[testIdxs,3])
+  tmp <- deleteNewIDs(testA,trainUsers,trainItems)  # see note, xvalMM.R 
+  testA <- tmp$testSet
+  deleted <- tmp$deleted
+  tmp <- system.time(
+     pred <- predict(ryout,testA[,-ncol(testA)]) 
+  )
+  if (printTimes) cat('validation time: ',tmp,'\n')
+  # calculate accuracy 
+  result = list(nFullData=nrowRatIn,holdout=holdout,preds=pred,
+     deleted=deleted)
+  # accuracy measures
+  exact <- mean(round(pred) == testA[,3],na.rm=TRUE)
+  mad <- mean(abs(pred-testA[,3]),na.rm=TRUE)
+  rms= sqrt(mean((pred-testA[,3])^2,na.rm=TRUE))
+  # if just guess mean
+  meanRat <- mean(testA[,3],na.rm=TRUE)
+  overallexact <- 
+     mean(round(meanRat) == testA[,3],na.rm=TRUE)
+  overallmad <- mean(abs(meanRat-testA[,3]),na.rm=TRUE)
+  overallrms <- sd(testA[,3],na.rm=TRUE)  
+  result$acc <- list(exact=exact,mad=mad,rms=rms,
+     overallexact=overallexact,
+     overallmad=overallmad,
+     overallrms=overallrms)
+  result$idxs <- testIdxs
+  result$preds <- pred
+  result$actuals <- testA[,3]
+  result$type <- 'dn'
+  class(result) <- 'xvalb'
+  result
+}
+
+# any users or items in test set but not the training set?
+deleteNewIDs <- function(testSet,trainUsers,trainItems)
+{
+   deleted <- NULL  # named row numbers from the original full data
+   rns <- row.names(testSet)
+   tmp <- setdiff(unique(testSet[,1]),unique(trainUsers))
+   if (length(tmp) > 0) {
+      for (usr in tmp) {
+         tmp1 <- which(testSet[,1] == usr)
+         # tmp1 is ordinal row numbers within testSet; the latter may
+         # have shrunken in earlier iterations!
+         deleted <- c(deleted,row.names(testSet[tmp1,]))
+         testSet <- testSet[-tmp1,]
+      }
+   }
+   tmp <- setdiff(unique(testSet[,2]),unique(trainItems))
+   if (length(tmp) > 0) {
+      for (itm in tmp) {
+         tmp1 <- which(testSet[,2] == itm)
+         # tmp1 is ordinal row numbers within testSet; the latter may
+         # have shrunken in earlier iterations, here or above!
+         deleted <- c(deleted,row.names(testSet[tmp1,]))
+         testSet <- testSet[-tmp1,]
+      }
+   }
+   deleted <- unique(deleted)
+   list(testSet = testSet, deleted = deleted)
+}
+
+# check
+checkxv <- function() {
+   set.seed(999999)
+   check <- data.frame(
+      u=sample(1:5,12,replace=TRUE),
+      i=sample(11:15,12,replace=TRUE),
+      r=sample(21:25,12,replace=TRUE))
+   print(check) 
+   xvout <- xvalMM(check,0.5)
+   print(xvout$idxs)  # 1 6 7 8 11 12
+   print(xvout$deleted)  # "8" "11"
+   print(xvout$preds)
+   print(xvout$actuals)
+   check$cv = sample(31:35,12,replace=TRUE)  # covariate
+   print(check)
+   print(xvout)
+}
+
+# see how well covs do on small Ni users
+xvSmallNi <- function(ratIn,maxN,minN) {
+   ri1 <- as.character(ratIn[,1])
+   NiVals <- tapply(ri1,ri1,length)
+   smallNi <- which(NiVals <= maxN)
+   rows <- as.numeric(names(smallNi))
+   smallRatIn <- ratIn[rows,]
+   xvalMM(smallRatIn)$acc
+}
