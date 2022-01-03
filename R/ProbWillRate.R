b943d9fd360dfd738f21e0519c29bb300fb06ed3
matloff <matloff@cs.ucdavis.edu>
2022-01-02 21:31:36 -0800

    mainly add asMatrix

--- R/ProbWillRate.R
+++ R/ProbWillRate.R
@@ -2,6 +2,8 @@
 # goal of this file:  determine the estimated probability that this user
 # will rate this item
 
+# UNDER CONSTRUCTION
+
 # arguments:
 
 #    bigUIRandCovs:  e.g. output of getML100K(); format is
@@ -11,26 +13,26 @@
 #       (user,item) combinations not present in the data; rating column
 #       is now 1 for rating present, 0 for not; useritem covs removed
 
-getFullUIRandCovs <- function(bigUIRandCovs) 
-{
-   # as usual in this pkg, user, item IDs assumed consecutive from 1
-   users <- 1:max(bigUIRandCovs$user)  
-   items <- 1:max(bigUIRandCovs$item)  
-   itemsByUser <- split(bigUIRandCovs$items,users)
-   userCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'userCovs')][1,]
-   itemCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'itemCovs')][1,]
-
-   doOneUser <- function(oneUser)  # one elt of itemsByUser
-   {
-      missingItems <- setdiff(items,itemsByUser)
-      usercovs <- userCovs[oneuser[1,]
-      # confusing to do *apply() within *apply()
-      for (missingitem in missingItems) {
-      }
-   }
-}
-
-probWillRate <- function(bigUIRandCovs,user,item) 
-{
-
-}
+## getFullUIRandCovs <- function(bigUIRandCovs) 
+## {
+##    # as usual in this pkg, user, item IDs assumed consecutive from 1
+##    users <- 1:max(bigUIRandCovs$user)  
+##    items <- 1:max(bigUIRandCovs$item)  
+##    itemsByUser <- split(bigUIRandCovs$items,users)
+##    userCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'userCovs')][1,]
+##    itemCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'itemCovs')][1,]
+## 
+##    doOneUser <- function(oneUser)  # one elt of itemsByUser
+##    {
+##       missingItems <- setdiff(items,itemsByUser)
+##       usercovs <- userCovs[oneuser[1,]
+##       # confusing to do *apply() within *apply()
+##       for (missingitem in missingItems) {
+##       }
+##    }
+## }
+## 
+## probWillRate <- function(bigUIRandCovs,user,item) 
+## {
+## 
+## }
--- R/Utils.R
+++ R/Utils.R
@@ -225,6 +225,56 @@ getCatPrefs <- function(usrInfo)
    res
 }
 
+
+# goal of this file:  determine the estimated probability that this user
+# will rate this item
+
+# arguments:
+
+#    bigUIRandCovs:  e.g. output of getML100K(); format is
+#       (userID; itemID; rating; useritem covs; user covs; item covs)`
+
+#    value: data frame of the same format, but with rows added for the
+#       (user,item) combinations not present in the data; rating column
+#       is now 1 for rating present, 0 for not; useritem covs removed
+
+getFullUIRandCovs <- function(bigUIRandCovs) 
+{
+###    # as usual in this pkg, user, item IDs assumed consecutive from 1
+###    users <- 1:max(bigUIRandCovs$user)  
+###    items <- 1:max(bigUIRandCovs$item)  
+###    itemsByUser <- split(bigUIRandCovs$items,users)
+###    userCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'userCovs')][1,]
+###    itemCovs <- bigUIRandCovs[,attr(bigUIRandCovs,'itemCovs')][1,]
+### 
+###    doOneUser <- function(oneUser)  # one elt of itemsByUser
+###    {
+###       missingItems <- setdiff(items,itemsByUser)
+###       usercovs <- userCovs[oneuser[1,]
+###       # confusing to do *apply() within *apply()
+###       for (missingitem in missingItems) {
+###       }
+###    }
+    users <- 1:max(bigUIRandCovs$user)
+    items <- 1:max(bigUIRandCovs$item)
+    itemsByUser <- split(bigUIRandCovs$items, users)
+    userCovs <- bigUIRandCovs[, attr(bigUIRandCovs, "userCovs")][1, 
+        ]
+    itemCovs <- bigUIRandCovs[, attr(bigUIRandCovs, "itemCovs")][1, 
+        ]
+    doOneUser <- function(oneUser) {
+        missingItems <- setdiff(items, itemsByUser)
+        usercovs <- userCovs[oneuser, ]
+        for (missingitem in missingItems) {
+        }
+    }
+
+}
+
+probWillRate <- function(bigUIRandCovs,user,item) 
+{
+
+}
 getMovieLensGenres <- function() 
 {
    movs <- read.table('u.item',sep='|')
@@ -236,3 +286,18 @@ getMovieLensUserPrefs <- function()
 
 }
 
+########################  asMatrix(input()  ##############################
+
+# userData is output of formUserData()
+
+asMatrix <- function(userData) 
+{
+   nr <- max(as.numeric(names(userData)))
+   nc <- max(sapply(userData,function(elt) max(elt$itms)))
+   res <- matrix(nrow=nr,ncol=nc,NA)
+   for (i in 1:nr) {
+      rw <- userData[[i]]
+      res[i,rw$itms] <- rw$ratings
+   }
+   res
+}
