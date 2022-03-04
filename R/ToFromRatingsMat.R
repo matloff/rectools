f1c7b060eb52bff5e556b2cf7d64d462b7a6920c
matloff <matloff@cs.ucdavis.edu>
2022-03-03 23:00:23 -0800

    toUserItemRatings() now can use row, col names from the input

--- R/ToFromRatingsMat.R
+++ R/ToFromRatingsMat.R
@@ -46,13 +46,15 @@ buildMatrix <- function(ratingsIn,NAval=0){
 # arguments:
 
 #    ratMat:  ratings matrix, users in rows, items in cols, NAs where
-#             unavailable 
+#       unavailable 
+#    rowColNames:  if TRUE, use row, col names of ratMat in output 
+#       userID, itemID columns; else, use row, col numbers in these
+#       output columns
 
 # value:  the (user ID, # item ID, rating) data frame; user and item IDs
 # come from row and column numbers in the ratings matrix
 
-
-toUserItemRatings <- function(ratMat) 
+toUserItemRatings <- function(ratMat,rowColNames=FALSE) 
 {
    # might use various reshape versions, but making it explicit will
    # allow extensions
@@ -61,18 +63,45 @@ toUserItemRatings <- function(ratMat)
    m <- ncol(ratMat)
    outDF <- NULL
 
+   # determine row, col names; create them if they're not there
+   rns <- rownames(ratMat)
+   cns <- colnames(ratMat) 
+   if (length(rns) == 0) {
+      inputHasRownames <- FALSE
+      if (rowColNames) rns <- 1:n
+   } else inputHasRownames <- TRUE
+   if (length(cns) == 0) {
+      inputHasColnames <- FALSE
+      if (rowColNames) cns <- 1:m
+   } else inputHasColnames <- TRUE
+
+   rnsF <- as.factor(rns)
+   cnsF <- as.factor(cns)
+   rnsL <- levels(rnsF)
+   cnsL <- levels(cnsF)
+
    for (i in 1:n) {
       rw <- ratMat[i,]
       nonNA <- which(!is.na(rw))
       nNonNA <- length(nonNA)
       if (nNonNA > 0) {
-         toAppend <- data.frame(
-            userID=rep(i,nNonNA),
-            itemID=nonNA,
-            ratings=rw[nonNA])
+         if (rowColNames) {
+            toAppend <- data.frame(
+               userID=rep(rnsL[i],nNonNA),
+               itemID=cnsL[nonNA],
+               ratings=rw[nonNA])
+         } else {
+            toAppend <- data.frame(
+               userID=rep(i,nNonNA),
+               itemID=nonNA,
+               ratings=rw[nonNA])
+         }
          outDF <- rbind(outDF,toAppend)
       }
    }
+
    outDF
 }
+
+touir <- toUserItemRatings
 
